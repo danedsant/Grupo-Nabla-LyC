c119c9ff864e07a6d2c2c7b9fd4b693e99dfc09b
Luen101 <luisemillanb@gmail.com>
2026-07-13 14:09:22 -0400

    Actividad 3: Diseñar un lenguaje (subconjunto de Rust) y crear su lexer usando el metacompilador Flex.
    - Codigo fuente: lexer.l
    - Ejecutable: lexer_rust.exe
    - Programa de  prueba: programa_prueba.rs

---
+++ Tema 4/Actividad 3/programa_prueba.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut x = 10;
+    let y = 3.5;
+    if x > 5 {
+        x = x + 1;
+    } else {
+        x = x - 1;
+    }
+    let z = x * y;
+    return;
+}
+
+fn prueba() {
+    let a = 1$;
+    let b = @2;
+}
