1966161024ad00951c86ce5d31b97a16d2262cf5
Daniel Santamaria <daneduvsant@gmail.com>
2026-07-24 00:46:40 -0400

    correccion en implementacion de AST

--- Tema 5/Actividad 1/Implementacion_AST.py
+++ Tema 5/Actividad 1/Implementacion_AST.py
@@ -20,7 +20,7 @@ class OperacionBinaria(NodoAST):
     def __str__(self):
         return f"BinOp({self.operador})"
 
-# Definimos una función para imprimir el árbol estructurado de manera legible
+# Definicion una función para imprimir el árbol estructurado de manera legible
 def imprimir_arbol(nodo, nivel=0):
     indentacion = "  " * nivel
     
@@ -31,23 +31,22 @@ def imprimir_arbol(nodo, nivel=0):
         imprimir_arbol(nodo.izquierda, nivel + 1)
         imprimir_arbol(nodo.derecha, nivel + 1)
 
-    # Si es un identificador, simplemente lo imprimimos
+    # Si es un identificador, se imprime
     elif isinstance(nodo, Identificador):
         print(f"{indentacion}└─> {nodo}")
 
-# Impplementación de un ejemplo de uso del AST
-# simular lo que hace el parser al leer: a + b * c
-# Recordamos la precedencia: la multiplicación (b * c) se agrupa primero.
+# Implementación de un ejemplo de uso del AST
+# Simular lo que hace el parser al leer: a + b * c
 
-# Primero construimos el subárbol de la multiplicación
+# Primero se construye el subárbol de la multiplicación
 nodo_b = Identificador("b")
 nodo_c = Identificador("c")
 nodo_multiplicacion = OperacionBinaria(nodo_b, "*", nodo_c)
 
-# Luego construimos la raíz de la suma, pasando 'a' y el subárbol anterior
+# Luego se construye la raíz de la suma, pasando 'a' y el subárbol anterior
 nodo_a = Identificador("a")
 raiz_suma = OperacionBinaria(nodo_a, "+", nodo_multiplicacion)
 
-# Mostramos el resultado
+# Se muestra la estructura del AST resultante
 print("Estructura del AST para:  \na + b * c")
 imprimir_arbol(raiz_suma)
